ad87b15dde2ef9c6dc814b029aca6b84abfa0918
komori3 <yamai.fujino@gmail.com>
2022-09-02 23:32:27 +0900

    Point の関数を追加

--- src/point.cpp
+++ src/point.cpp
@@ -1,2 +1,25 @@
 #include "stdafx.h"
 #include "point.h"
+
+Point Point::getPoints() const { return { px, py }; }
+
+Point Point::getDiff(const Point& other) const {
+    return { std::max(0, px - other.px), std::max(0, py - other.py) };
+}
+
+bool Point::isStrictryInside(const Point& bottomLeft, const Point& topRight) const {
+  return bottomLeft.px < px&& px < topRight.px&& bottomLeft.py < py&& py < topRight.py;
+}
+
+bool Point::isOnBoundary(const Point& bottomLeft, const Point& topRight) const {
+  return (bottomLeft.px == px && bottomLeft.py <= py && py <= topRight.py)
+    || (topRight.px == px && bottomLeft.py <= py && py <= topRight.py)
+    || (bottomLeft.py == py && bottomLeft.px <= px && px <= topRight.px)
+    || (topRight.py == py && bottomLeft.px <= px && px <= topRight.px);
+}
+
+bool Point::isInside(const Point& bottomLeft, const Point& topRight) const {
+  return isStrictryInside(bottomLeft, topRight) || isOnBoundary(bottomLeft, topRight);
+}
+
+int Point::getScalarSize() const { return px * py; }
--- src/point.h
+++ src/point.h
@@ -4,4 +4,19 @@ struct Point
 {
   int px;
   int py;
+
+  Point(int px = 0, int py = 0) : px(px), py(py) {}
+
+  Point getPoints() const;
+
+  Point getDiff(const Point& other) const;
+
+  bool isStrictryInside(const Point& bottomLeft, const Point& topRight) const;
+
+  bool isOnBoundary(const Point& bottomLeft, const Point& topRight) const;
+
+  bool isInside(const Point& bottomLeft, const Point& topRight) const;
+
+  int getScalarSize() const;
+
 };
