3e3f58faa5a3452b3ffb6c14692120e7217f6f15
komori3 <yamai.fujino@gmail.com>
2022-09-03 00:35:11 +0900

    Canvas の中身を実装

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -1,2 +1,22 @@
 #include "stdafx.h"
 #include "canvas.h"
+
+Canvas::Canvas(int width, int height, const Color& backgroundColor)
+    : width(width), height(height), backgroundColor(backgroundColor)
+{
+    blocks["0"] = std::make_shared<SimpleBlock>("0", Point(0, 0), Point(width, height), backgroundColor);
+}
+
+Size Canvas::size() const {
+  return { width, height };
+}
+
+std::vector<std::shared_ptr<Block>> Canvas::simplify() const {
+  std::vector<std::shared_ptr<Block>> simplifiedBlocks;
+  for (const auto& [id, block] : blocks) {
+    for (const auto& child : block->getChildren()) {
+      simplifiedBlocks.push_back(child);
+    }
+  }
+  return simplifiedBlocks;
+}
--- src/canvas.h
+++ src/canvas.h
@@ -10,5 +10,16 @@ using Color = RGBA;
 
 struct Canvas
 {
+
+  int width;
+  int height;
+  Color backgroundColor;
   std::map<std::string, std::shared_ptr<Block>> blocks;
+
+  Canvas(int width, int height, const Color& backgroundColor);
+
+  Size size() const;
+
+  std::vector<std::shared_ptr<Block>> simplify() const;
+
 };
