99aaf3c78cf59d31fbd27f36a96d57707989c314
komori3 <yamai.fujino@gmail.com>
2022-09-03 01:20:37 +0900

    Frame::draw の定義を移動

--- src/painter.cpp
+++ src/painter.cpp
@@ -1,2 +1,19 @@
 #include "stdafx.h"
 #include "painter.h"
+
+Frame Painter::draw(const Canvas& canvas) {
+  const auto& blocks = canvas.simplify();
+  Frame frame(canvas.width * canvas.height);
+  int size = 0;
+  for (const auto& block : blocks) {
+    const Point frameTopLeft(block->bottomLeft.px, canvas.height - block->topRight.py);
+    const Point frameBottomRight(block->topRight.px, canvas.height - block->bottomLeft.py);
+    size += (frameBottomRight.px - frameTopLeft.px) * (frameBottomRight.py - frameTopLeft.py);
+    for (int y = frameTopLeft.py; y < frameBottomRight.py; y++) {
+      for (int x = frameTopLeft.px; x < frameBottomRight.px; x++) {
+        frame[y * canvas.width + x] = block->color;
+      }
+    }
+  }
+  return frame;
+}
--- src/painter.h
+++ src/painter.h
@@ -9,21 +9,6 @@ using Frame = std::vector<RGBA>;
 
 struct Painter {
 
-  static Frame draw(const Canvas& canvas) {
-    const auto& blocks = canvas.simplify();
-    Frame frame(canvas.width * canvas.height);
-    int size = 0;
-    for (const auto& block : blocks) {
-      const Point frameTopLeft(block->bottomLeft.px, canvas.height - block->topRight.py);
-      const Point frameBottomRight(block->topRight.px, canvas.height - block->bottomLeft.py);
-      size += (frameBottomRight.px - frameTopLeft.px) * (frameBottomRight.py - frameTopLeft.py);
-      for (int y = frameTopLeft.py; y < frameBottomRight.py; y++) {
-        for (int x = frameTopLeft.px; x < frameBottomRight.px; x++) {
-          frame[y * canvas.width + x] = block->color;
-        }
-      }
-    }
-    return frame;
-  }
+  static Frame draw(const Canvas& canvas);
 
 };
