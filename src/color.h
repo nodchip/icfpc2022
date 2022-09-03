07ba2198673d705fa724e829f310cdf6f39b62ca
Hitoshi Yoshida <github@peria.jp>
2022-09-03 14:08:29 +0900

    Fix build error

--- src/color.h
+++ src/color.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 struct RGBA {
   int r;
   int g;
@@ -7,41 +9,40 @@ struct RGBA {
   int a;
   RGBA(int r = 0, int g = 0, int b = 0, int a = 0);
   bool operator<(const RGBA& rhs) const noexcept {
-    if (r != rhs.r) return r < rhs.r;
-    if (g != rhs.g) return g < rhs.g;
-    if (b != rhs.b) return b < rhs.b;
+    if (r != rhs.r)
+      return r < rhs.r;
+    if (g != rhs.g)
+      return g < rhs.g;
+    if (b != rhs.b)
+      return b < rhs.b;
     return a < rhs.a;
   }
   bool operator==(const RGBA& rhs) const noexcept {
     return r == rhs.r && g == rhs.g && b == rhs.b && a == rhs.a;
   }
   bool operator!=(const RGBA& rhs) const noexcept { return !(*this == rhs); }
-  bool operator<(const RGBA& rhs) const noexcept {
-    if (r != rhs.r) {
-      return r < rhs.r;
-    }
-    if (g != rhs.g) {
-      return g < rhs.g;
-    }
-    if (b != rhs.b) {
-      return b < rhs.b;
-    }
-    return a < rhs.a;
-  }
   int operator[](std::size_t channel) const noexcept {
     switch (channel) {
-      case 0: return r;
-      case 1: return g;
-      case 2: return b;
-      default: return a;
+      case 0:
+        return r;
+      case 1:
+        return g;
+      case 2:
+        return b;
+      default:
+        return a;
     }
   }
   int& operator[](std::size_t channel) noexcept {
     switch (channel) {
-      case 0: return r;
-      case 1: return g;
-      case 2: return b;
-      default: return a;
+      case 0:
+        return r;
+      case 1:
+        return g;
+      case 2:
+        return b;
+      default:
+        return a;
     }
   }
 };
