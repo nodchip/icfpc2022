7646197a5e4562d17c37dc9085b7b07e6f4e015f
komori3 <yamai.fujino@gmail.com>
2022-09-04 09:59:25 +0900

    merge_all: 前段ある場合はスキップ / problem31-35 対応

--- src/solvers/merge_all.cpp
+++ src/solvers/merge_all.cpp
@@ -9,22 +9,25 @@ class MergeAll : public SolverBase {
 public:
   MergeAll() { }
   SolverOutputs solve(const SolverArguments &args) override {
+
     SolverOutputs ret;
 
-    // problem 26~30 限定かも
+    // 前段がある場合はスルー
+    if (!args.optional_initial_solution.empty()) {
+      return ret;
+    }
 
     auto canvas = args.previous_canvas->Clone();
 
-    // block id は 左下->右上, 列スキャン
+    // block id は 左下->右上, 列スキャン (problem 26~35 限定？)
     std::vector<std::shared_ptr<Instruction>> instructions;
 
-    if (canvas->blocks.size() == 100) {
-      // 40x40 のブロックが 100 個
-      // merge 100=[0,1],101=[100,2],102=[101,3],...
-      int top_id = 99;
+    for (int board_size : {10, 16, 20}) {
+      if (canvas->blocks.size() != board_size * board_size) continue;
+      int top_id = board_size * board_size - 1;
       std::vector<int> col_ids;
-      for (int col_begin_id = 0; col_begin_id < 100; col_begin_id += 10) {
-        std::vector<int> row_ids(10);
+      for (int col_begin_id = 0; col_begin_id < board_size * board_size; col_begin_id += board_size) {
+        std::vector<int> row_ids(board_size);
         std::iota(row_ids.begin(), row_ids.end(), col_begin_id);
         int dst_id = row_ids.front();
         for (int i = 1; i < row_ids.size(); i++) {
@@ -44,41 +47,7 @@ public:
       }
       instructions.push_back(std::make_shared<ColorInstruction>(std::to_string(top_id), RGBA(255, 255, 255, 255)));
     }
-    else if (canvas->blocks.size() == 400) {
-      // 20x20 のブロックが 400 個
-      int top_id = 399;
-      std::vector<int> col_ids;
-      for (int col_begin_id = 0; col_begin_id < 400; col_begin_id += 20) {
-        std::vector<int> row_ids(20);
-        std::iota(row_ids.begin(), row_ids.end(), col_begin_id);
-        int dst_id = row_ids.front();
-        for (int i = 1; i < row_ids.size(); i++) {
-          int src_id = row_ids[i];
-          auto inst = std::make_shared<MergeInstruction>(std::to_string(dst_id), std::to_string(src_id));
-          instructions.push_back(inst);
-          dst_id = ++top_id;
-        }
-        col_ids.push_back(dst_id);
-      }
-      int dst_id = col_ids.front();
-      for (int i = 1; i < col_ids.size(); i++) {
-        int src_id = col_ids[i];
-        auto inst = std::make_shared<MergeInstruction>(std::to_string(dst_id), std::to_string(src_id));
-        instructions.push_back(inst);
-        dst_id = ++top_id;
-      }
-      instructions.push_back(
-        std::make_shared<ColorInstruction>(
-          std::to_string(top_id),
-          //*meanColor(*args.painting, Point(0, 0), Point(args.painting->width, args.painting->height))
-          //*geometricMedianColor(*args.painting, Point(0, 0), Point(args.painting->width, args.painting->height))
-          RGBA(255, 255, 255, 255)
-          )
-      );
-    }
 
-    // dummy.
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
     ret.solution = instructions;
     return ret;
 
